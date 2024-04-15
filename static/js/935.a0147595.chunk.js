d4cbd2ce83678202f5c1cf7715a16b1e050c418b
AndreaDiaz23 <133728008+AndreaDiaz23@users.noreply.github.com>
2024-04-15 18:41:22 +0000

    Deploying to gh-pages from @ AndreaDiaz23/goit-react-hw-05-movies@7f2e3e4a27e4aec0115b2678005de1ce30599fe8 🚀

---
+++ static/js/935.a0147595.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[935],{935:function(e,t,r){r.r(t),r.d(t,{default:function(){return o}});var n=r(689),c=r(87),a="styles_ul__1KtR7",i=r(184),o=function(e){var t=e.movies,r=e.route,o=(0,n.TH)();return(0,i.jsx)("ul",{className:a,children:t.map((function(e){var t=r?"movies/".concat(e.id):"".concat(e.id);return(0,i.jsx)("li",{children:(0,i.jsx)(c.rU,{to:t,state:{from:o},children:e.title})},e.id)}))})}}}]);
+//# sourceMappingURL=935.a0147595.chunk.js.map
